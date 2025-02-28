e41f1efef7c600881f41506546cbc0cec2c59de6
reisaraujo-miguel <reisaraujo.miguel@gmail.com>
2025-02-28 18:54:10 +0000

    fix: fix astronvim config and bashrc_append
    - bashrc_append: only run snippet if bash is in iteractive mode.
    - AstroNvim: I was importing too many language packs before, first boot
      of nvim was taking too long and with too many erros.
    - add devcontainer file for devpod

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -5,7 +5,6 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
 
   { import = "astrocommunity.colorscheme.catppuccin" },
@@ -18,14 +17,14 @@ return {
   { import = "astrocommunity.utility.hover-nvim" },
 
   -- Language packs
-  { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.cpp" },
-  { import = "astrocommunity.pack.cmake" },
-  { import = "astrocommunity.pack.go" },
-  { import = "astrocommunity.pack.docker" },
-  { import = "astrocommunity.pack.markdown" },
-  { import = "astrocommunity.pack.typescript-all-in-one" },
-  { import = "astrocommunity.pack.python" },
-  { import = "astrocommunity.pack.php" },
+  -- { import = "astrocommunity.pack.markdown" },
+  -- { import = "astrocommunity.pack.bash" },
+  -- { import = "astrocommunity.pack.cpp" },
+  -- { import = "astrocommunity.pack.cmake" },
+  -- { import = "astrocommunity.pack.go" },
+  -- { import = "astrocommunity.pack.docker" },
+  -- { import = "astrocommunity.pack.typescript-all-in-one" },
+  -- { import = "astrocommunity.pack.python" },
+  -- { import = "astrocommunity.pack.php" },
 }
--- bashrc_append.sh
+++ bashrc_append.sh
@@ -1,7 +1,8 @@
-if [ -z "$PS1" ]; then
-	return
+# Check if bash is running in interactive mode and not as a login shell
+if [[ $- == *i* ]] && [[ -z "$BASH_EXECUTION_STRING" ]] && [[ "$0" == *bash ]]; then
+    # Check if zsh is installed
+    if command -v zsh >/dev/null 2>&1; then
+        exec zsh
+    fi
 fi
 
-if [[ $- == *i* ]]; then
-	exec zsh
-fi
