06ecd4dc70205ed471229f1e399691d8265fa2bc
reisaraujo-miguel <reisaraujo.miguel@gmail.com>
2025-03-17 20:37:05 +0000

    fix extension sourcing

--- .config/zsh/.zsh.d/source-extensions.zsh
+++ .config/zsh/.zsh.d/source-extensions.zsh
@@ -1,16 +1,22 @@
 # Source autosuggestions if available
-if [ -f "/usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh" ]; then 
-	source "/usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
-elif [ -f "/home/linuxbrew/.linuxbrew/bin/zsh-autosuggestions.zsh" ]; then
-	source "/home/linuxbrew/.linuxbrew/bin/zsh-autosuggestions.zsh"
+SYSTEM_PACKAGE="/usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
+BREW_PACKAGE="/home/linuxbrew/.linuxbrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
+
+if [ -f $SYSTEM_PACKAGE ]; then 
+	source $SYSTEM_PACKAGE
+elif [ -f $BREW_PACKAGE ]; then
+	source $BREW_PACKAGE
 fi
 
 # Configure autosuggestions
 export ZSH_AUTOSUGGEST_STRATEGY=(match_prev_cmd history)
 
 # source syntax highlighting if available
-if [ -f "/usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ]; then
-	source "/usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
-elif [ -f "/home/linuxbrew/.linuxbrew/bin/zsh-syntax-highlighting.zsh" ]; then
-	source "/home/linuxbrew/.linuxbrew/bin/zsh-syntax-highlighting.zsh"
+SYSTEM_PACKAGE="/usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" 
+BREW_PACKAGE="/home/linuxbrew/.linuxbrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" 
+
+if [ -f $SYSTEM_PACKAGE ]; then
+	source $SYSTEM_PACKAGE
+elif [ -f $BREW_PACKAGE ]; then
+	source $BREW_PACKAGE
 fi
