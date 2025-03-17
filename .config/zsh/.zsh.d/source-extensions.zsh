7f554475f67a08f12ec3fd48707ba5ebc40159bc
reisaraujo-miguel <reisaraujo.miguel@gmail.com>
2025-03-17 18:31:09 +0000

    fix space missin

--- .config/zsh/.zsh.d/source-extensions.zsh
+++ .config/zsh/.zsh.d/source-extensions.zsh
@@ -9,7 +9,7 @@ fi
 export ZSH_AUTOSUGGEST_STRATEGY=(match_prev_cmd history)
 
 # source syntax highlighting if available
-if [ -f "/usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"]; then
+if [ -f "/usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ]; then
 	source "/usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 elif [ -f "/home/linuxbrew/.linuxbrew/bin/zsh-syntax-highlighting.zsh" ]; then
 	source "/home/linuxbrew/.linuxbrew/bin/zsh-syntax-highlighting.zsh"
